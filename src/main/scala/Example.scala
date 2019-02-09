53d4d1097593306b09d78658d5bfbea23237b4d1
robertbutacu <17516693+robertbutacu@users.noreply.github.com>
2019-02-09 21:19:21 +0000

    experimenting a bit

---
+++ src/main/scala/Example.scala
@@ -0,0 +1,7 @@
+
+case class Example(a: String, b: String, c: Int, other: Other)
+
+case class Other(a: Double, c: Char, otherOther: OtherOther)
+
+case class OtherOther(c: String, e: String)
+
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,16 @@
-class Main {
+import shapeless._
 
+object Main extends App {
+  val otherOther = OtherOther("asdfa", "asdasd")
+
+  val other = Other(2.0, 'c', otherOther)
+
+  val example = Example("asdasd", "asdasd", 5, other)
+  val genericOtherOther: String :: String :: HNil = Generic[OtherOther].to(otherOther)
+
+  val genericOther = Generic[Other].to(other)
+
+  val genericExample = Generic[Example].to(example)
+
+  println(the[Generic[Example]].to(example))
 }
